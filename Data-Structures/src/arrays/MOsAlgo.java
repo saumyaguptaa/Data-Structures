e17976855d3418024aa9066dbebe7842d9f2b514
saumyaguptaa <saumyagupta431997@gmail.com>
2018-09-11 15:29:45 +0530

    Missed file

---
+++ Data-Structures/src/arrays/MOsAlgo.java
@@ -0,0 +1,18 @@
+package arrays;
+
+public class MOsAlgo {
+	public static void main(String[] args) {
+		int[] arr = {1, 1, 2, 1 ,3, 4, 5, 2, 8};
+		int[] query = {1,2,3,2,2,6};
+		int i, j, begin, end, sum = 0;
+		for(i = 0; i< query.length; i=i+2) {
+			begin = query[i];
+			end = query[i+1];
+			for(j = begin; j<= end; j++) {
+				sum += arr[j];
+				System.out.println(sum);
+			}
+			sum = 0;
+		}			
+	}
+}	
